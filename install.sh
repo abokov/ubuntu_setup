5f21dc455b80357ed1e8fa8d55d7e664f7f27dad
Alexey Bokov <bokov@bokov.net>
2020-05-06 14:53:53 -0700

    install packages bash

---
+++ install.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+# Azure part
+sudo apt install azure-cli
+
+#Conda install ( note - will replace/install python 3.6  )
+wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+chmod 777  Anaconda3-2020.02-Linux-x86_64.sh
+./Anaconda3-2020.02-Linux-x86_64.sh
+
+
